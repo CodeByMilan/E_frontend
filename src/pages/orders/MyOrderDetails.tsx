77caf69c3811320b90ac98a1d1f7ca2a66eb6017
Milan Acharya <milanacharya1614@gmail.com>
2024-11-23 14:31:28 +0545

    order details page added

--- src/pages/orders/MyOrderDetails.tsx
+++ src/pages/orders/MyOrderDetails.tsx
@@ -11,6 +11,7 @@ const MyOrderDetails = () => {
     const {id} =useParams()
     const dipatch = useAppDispatch()
     const {myOrderDetails}=useAppSelector((state)=>state.order)
+    console.log(myOrderDetails)
 
     useEffect(()=>{
         if(id){
@@ -25,37 +26,39 @@ const MyOrderDetails = () => {
     
     <div className="flex justify-start item-start space-y-5 flex-col">
       <h1 className="text-1xl dark:text-white lg:text-2xl font-semibold leading-7 lg:leading-9 text-gray-600">Order Details </h1>
-      <p className="text-base dark:text-gray-300 font-medium leading-6 text-gray-600">{new Date(myOrderDetails[0]?.Order?.createdAt).toLocaleDateString}</p>
+      <p className="text-base dark:text-gray-300 font-medium leading-6 text-gray-600">{new Date(myOrderDetails[0]?.Order?.createdAt).toLocaleDateString
+        ()}</p>
     </div>
     <div className="mt-10 flex flex-col xl:flex-row jusitfy-center items-stretch w-full xl:space-x-8 space-y-4 md:space-y-6 xl:space-y-0">
       <div className="flex flex-col justify-start items-start w-full space-y-4 md:space-y-6 xl:space-y-8">
         <div className="flex flex-col justify-start items-start dark:bg-gray-800 bg-gray-50 px-4 py-4 md:py-6 md:p-6 xl:p-8 w-full">
           <p className="text-lg md:text-xl dark:text-white font-semibold leading-6 xl:leading-5 text-gray-800">My Order</p>
 {
-    myOrderDetails.leng
+    myOrderDetails.length >0 && myOrderDetails.map((item)=>{
+      return(
+
+        <div className="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full" key ={item?.Order?.id}>
+        <div className="pb-4 md:pb-8 w-full md:w-40">
+          <img className="w-full hidden md:block"  src={item?.Product?.productImageUrl}alt="dress" />
+          
+        </div>
+          <p className="text-base dark:text-white xl:text-lg leading-6">{item?.Product?.productName} </p>
+        <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
+          <div className="w-full flex flex-col justify-start items-start space-y-8">
+            <h3 className="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800"></h3>
+          </div>
+          <div className="flex justify-between space-x-8 items-start w-full">
+            <p className="text-base dark:text-white xl:text-lg leading-6">Rs. {item?.Product?.price}</p>
+            <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">Qty:{item.quantity}</p>
+            <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">Rs. {item.Product.price*item.quantity} </p>
+          </div>
+        </div>
+      </div>
+      )
 }
-           
-                  <div className="mt-4 md:mt-6 flex flex-col md:flex-row justify-start items-start md:items-center md:space-x-6 xl:space-x-8 w-full" >
-                  <div className="pb-4 md:pb-8 w-full md:w-40">
-                    <img className="w-full hidden md:block"  src=""alt="dress" />
-                    
-                  </div>
-                    <p className="text-base dark:text-white xl:text-lg leading-6"> </p>
-                  <div className="border-b border-gray-200 md:flex-row flex-col flex justify-between items-start w-full pb-8 space-y-4 md:space-y-0">
-                    <div className="w-full flex flex-col justify-start items-start space-y-8">
-                      <h3 className="text-xl dark:text-white xl:text-2xl font-semibold leading-6 text-gray-800"></h3>
-                    </div>
-                    <div className="flex justify-between space-x-8 items-start w-full">
-                      <p className="text-base dark:text-white xl:text-lg leading-6">Rs. </p>
-                      <p className="text-base dark:text-white xl:text-lg leading-6 text-gray-800">Qty:</p>
-                      <p className="text-base dark:text-white xl:text-lg font-semibold leading-6 text-gray-800">Rs.  </p>
-                    </div>
-                  </div>
-                </div>
-             
-            
-    
-     
+    )
+  }
+
         </div>
         <div className="flex justify-center flex-col md:flex-row flex-col items-stretch w-full space-y-4 md:space-y-0 md:space-x-6 xl:space-x-8">
           <div className="flex flex-col px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
@@ -64,20 +67,20 @@ const MyOrderDetails = () => {
 
               <div className="flex justify-between items-center w-full">
                 <p className="text-base dark:text-white leading-4 text-gray-800">Payment Method</p>
-                <p className="text-base dark:text-gray-300 leading-4 text-gray-600"></p>
+                <p className="text-base dark:text-gray-300 leading-4 text-gray-600">{myOrderDetails[0]?.Order?.Payment?.paymentMethod}</p>
               </div>
               <div className="flex justify-between items-center w-full">
                 <p className="text-base dark:text-white leading-4 text-gray-800">Payment Status</p>
-                <p className="text-base dark:text-gray-300 leading-4 text-gray-600"></p>
+                <p className="text-base dark:text-gray-300 leading-4 text-gray-600">{myOrderDetails[0]?.Order?.Payment?.paymentStatus}</p>
               </div>
               <div className="flex justify-between items-center w-full">
                 <p className="text-base dark:text-white leading-4 text-gray-800">Order Status</p>
-                <p className="text-base dark:text-gray-300 leading-4 text-gray-600"></p>
+                <p className="text-base dark:text-gray-300 leading-4 text-gray-600">{myOrderDetails[0]?.Order?.orderStatus}</p>
               </div>
             </div>
             <div className="flex justify-between items-center w-full">
               <p className="text-base dark:text-white font-semibold leading-4 text-gray-800">Total</p>
-              <p className="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600"></p>
+              <p className="text-base dark:text-gray-300 font-semibold leading-4 text-gray-600">{myOrderDetails[0]?.Order?.totalAmount}</p>
             </div>
           </div>
           <div className="flex flex-col justify-center px-4 py-6 md:p-6 xl:p-8 w-full bg-gray-50 dark:bg-gray-800 space-y-6">
@@ -104,8 +107,8 @@ const MyOrderDetails = () => {
           <div className="flex justify-between xl:h-full items-stretch w-full flex-col mt-6 md:mt-0">
             <div className="flex justify-center md:justify-start xl:flex-col flex-col md:space-x-6 lg:space-x-8 xl:space-x-0 space-y-4 xl:space-y-12 md:space-y-0 md:flex-row items-center md:items-start">
               <div className="flex justify-center md:justify-start items-center md:items-start flex-col space-y-4 xl:mt-8">
-                <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">Address : </p>
-                <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">Phone :  </p>
+                <p className="text-base dark:text-white font-semibold leading-4 text-center md:text-left text-gray-800">Address : {myOrderDetails[0]?.Order?.shippingAddress}</p>
+                <p className="w-48 lg:w-full dark:text-gray-300 xl:w-48 text-center md:text-left text-sm leading-5 text-gray-600">Phone : {myOrderDetails[0].Order?.phoneNumber} </p>
               </div>
 
             </div>
--- src/pages/orders/MyOrders.tsx
+++ src/pages/orders/MyOrders.tsx
@@ -104,7 +104,7 @@ export const MyOrders = () => {
                                            </Link>
                                         </td>
                                         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-                                            <p className="text-gray-900 whitespace-no-wrap">{order?.totalAmount}</p>
+                                            <p className="text-gray-900 whitespace-no-wrap">{order?.totalAmount+100}</p>
                                         </td>
                                         <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
                                             <p className="text-gray-900 whitespace-no-wrap">
